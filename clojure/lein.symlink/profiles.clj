96178db527668b9ff828c09aefdad3b01fb853b5
Nicolas Berger <nicoberger@gmail.com>
2015-04-10 14:37:44 -0300

    Cleanup in lein profiles

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -2,6 +2,4 @@
         :dependencies [[pjstadig/humane-test-output "0.6.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]
-        :plugins [[lein-droid "0.2.0"]
-                  [cider/cider-nrepl "0.7.0"]]
-        :android {:sdk-path "/home/nicolas/tools/android/sdk"}}}
+        :plugins [[cider/cider-nrepl "0.8.2"]]}}
