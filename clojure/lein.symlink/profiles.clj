bf07555eed1f9462e3e6ea88092a5eda050c1e52
Nicolas Berger <nicoberger@gmail.com>
2015-06-21 19:41:50 -0300

    Add some lein plugins

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -1,5 +1,12 @@
 {:user {
-        :dependencies [[pjstadig/humane-test-output "0.6.0"]]
+        :dependencies [[pjstadig/humane-test-output "0.6.0"]
+                       [slamhound "1.5.5"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]
-        :plugins [[cider/cider-nrepl "0.8.2"]]}}
+
+        :plugins [
+                  [lein-kibit "0.1.2"]
+                  [lein-try "0.4.3"]
+                  [cider/cider-nrepl "0.8.2"]
+                  [lein-ancient "0.6.7"]
+                  [lein-pprint "1.1.1"]]}}
