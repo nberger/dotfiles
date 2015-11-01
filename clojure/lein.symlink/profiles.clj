d25e1bf73638516a907dba97459bdb90bc5f6838
Nicolas Berger <nicoberger@gmail.com>
2015-11-01 10:12:31 -0300

    Lein: Add lib-dev profile, for easier release to clojars

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -10,4 +10,19 @@
                   [lein-try "0.4.3"]
                   [cider/cider-nrepl "0.8.2"]
                   [lein-ancient "0.6.7"]
-                  [lein-pprint "1.1.1"]]}}
+                  [lein-pprint "1.1.1"]]}
+ :lib-dev {
+           :gsub-version/files #{"README.md"}
+           :release-tasks [["clean"]
+                           ["vcs" "assert-committed"]
+                           ["change" "version"
+                            "leiningen.release/bump-version" "release"]
+                           ["gsub-version"]
+                           ["vcs" "commit"]
+                           ["vcs" "tag"]
+                           ["deploy"]
+                           ["change" "version" "leiningen.release/bump-version"]
+                           ["vcs" "commit"]
+                           ["vcs" "push"]]
+           :plugins [
+                  [com.gfredericks/corncob-cigar "0.1.5"]]}}
