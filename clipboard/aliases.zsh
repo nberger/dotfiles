2c31b748f202efb34b6a1356800ba124bb1d7409
Nicolas Berger <nicoberger@gmail.com>
2017-04-07 14:16:01 -0700

    Make delete-local-merged and pbcopy/paste to work in OSX

--- clipboard/aliases.zsh
+++ clipboard/aliases.zsh
@@ -1,2 +1,7 @@
-alias pbcopy='xclip -selection clipboard'
-alias pbpaste='xclip -selection clipboard -o'
+if [[ `uname` == 'Darwin' ]]
+then
+  # nothing to do, pbcopy and pbpaste just work
+else
+  alias pbcopy='xclip -selection clipboard'
+  alias pbpaste='xclip -selection clipboard -o'
+fi
