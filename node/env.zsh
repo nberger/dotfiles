c49a2d9805919cbfcfb25c93eb709a5b90355a2a
Nicolas Berger <nicoberger@gmail.com>
2017-10-17 17:49:29 -0300

    Remove tweaks for homebrew-installed node
    I install node (nvm) via script now

--- node/env.zsh
+++ node/env.zsh
@@ -1,12 +1,6 @@
 export NVM_DIR="$HOME/.nvm"
 if [ -s "$NVM_DIR/nvm.sh" ] ; then
   . $NVM_DIR/nvm.sh --no-use  # This loads nvm
-elif brew --prefix nvm > /dev/null; then
-  echo "loading nvm.sh from brew"
-  source $(brew --prefix nvm)/nvm.sh --no-use
 fi
-
-alias node='unalias node ; unalias npm ; nvm use default ; node $@'
-alias npm='unalias node ; unalias npm ; nvm use default ; npm $@'
 
 export PATH=$PATH:$NVM_BIN
